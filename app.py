15a44cdd860fff573fe7eedd363c1d868911696a
Daniel Callejas Sevilla <daniel.callejas.sevilla@gmail.com>
2018-08-21 23:02:10 +0200

    Parse opening_hours and show correct status for current time; also, accept parameters instead of weird URL format

--- app.py
+++ app.py
@@ -4,21 +4,36 @@
 #  $ FLASK_APP=test.py flask run
 
 import overpy
-from flask import Flask, render_template
+from osm_time.opening_hours import OpeningHours
+from flask import Flask, render_template, request
+import datetime
 app = Flask(__name__)
 
-@app.route("/", defaults={'rect': '43.572,7.014,43.578,7.026'})
-@app.route("/rect:<rect>")
-def hours(rect):
+DAYS = ['mo', 'tu', 'we', 'th', 'fr', 'sa', 'su'] # OSM opening_hours format
+
+@app.route("/")
+def hours():
+    rect = request.args.get('rect', '43.572,7.014,43.578,7.026')
+    when = request.args.get('when', 'now')
+
     o = overpy.Overpass()
     q = "node[opening_hours]({});out;".format(rect) # TODO filter rect before injecting
     r = o.query(q)
 
-    results = []    
+    results = []
+    
+    if when == "now":
+        now = datetime.datetime.today()    
+        today = DAYS[now.weekday()]
+        now = now.strftime("%H:%M")
+    else:
+        today, now = when.split(" ")
+    print(today, now)
     for node in r.nodes:
         item = { 'name': node.tags['name'],
                  'url': "https://www.openstreetmap.org/node/{}".format(node.id),
                  'hours': node.tags['opening_hours'].split(';'),
+                 'open': OpeningHours(node.tags['opening_hours']).is_open(today, now),
                }
         results.append(item)
 
