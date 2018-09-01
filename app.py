fede7373bd776e1c71f9b4525943fe81f8c54140
Daniel Callejas Sevilla <daniel.callejas.sevilla@gmail.com>
2018-09-01 23:14:46 +0200

    Added column for type of store; Use a center lat,lon instead of a rect; WIP: ability to pin places

--- app.py
+++ app.py
@@ -2,19 +2,75 @@
 
 import overpy
 from osm_time.opening_hours import OpeningHours, ParseException
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, make_response
 import datetime
 app = Flask(__name__)
 
 DAYS = ['mo', 'tu', 'we', 'th', 'fr', 'sa', 'su'] # OSM opening_hours format
 
+def getPinned():
+    current = request.cookies.get('nodeIds')
+    
+    if current:
+        return set(map(string_as_int_or_zero, current.split(",") ) )
+    else:
+        return set()
+
+@app.route("/recordar", methods=['POST']) # TODO POST with a URL param?
+def remember():
+    current = getPinned()
+    print(f"Current: {current}")
+    new = string_as_int_or_zero(request.args.get('nodeId'))
+    current.add(new)
+    
+    resp = make_response("ok")
+    
+    resp.set_cookie('nodeIds', ",".join( (str(nodeId) for nodeId in current) ) )
+    
+    return resp
+
+def string_as_int_or_zero(s):
+    try:
+        return int(s)
+    except ValueError as e:
+        return 0
+
+def makeRect(center, radius):
+    """
+       center is a latitude, longitude string in the format "<float>,<float>"
+       radius is a float
+       
+       returns a string containing "lat-radius, lon-radius, lat+radius, lon+radius"
+    """
+    lat,lon = center.split(",")
+    lat,lon = float(lat),float(lon)
+    
+    return f"{lat-radius},{lon-radius},{lat+radius},{lon+radius}"
+
+def getTypeFromTags(tags):
+    """
+    Guesses a meaningful type of OSM node/way based on the tags
+    by picking one from amenity, shop, ...
+    """
+    keys = tags.keys()
+    if   "amenity" in keys:
+        return tags["amenity"]
+    elif "shop" in keys:
+        return tags["shop"]
+        
+    return "unknown"
+
 @app.route("/")
 def hours():
-    rect = request.args.get('rect', '43.572,7.014,43.578,7.026')
+#    rect = request.args.get('rect', '43.572,7.014,43.578,7.026')
+    center = request.args.get('center', '43.5744,7.0192')
+    radius = 0.005
+    rect = makeRect(center, radius)
     when = request.args.get('when', 'now')
-
+    pinned = getPinned()
+    
     o = overpy.Overpass()
-    q = f"node[opening_hours]({rect});out;" # TODO filter rect before injecting
+    q = f"node[opening_hours]({rect});out;"   # TODO retrieve also ways (eg. a whole building is a shop)
     r = o.query(q)
 
     if when == "now":
@@ -34,10 +90,12 @@ def hours():
         except ParseException as e:
             open = 'unknown'
            
-        item = { 'name': node.tags['name'],
+        item = { 'name': node.tags.get('name'),
                  'url': f"https://www.openstreetmap.org/node/{node.id}",
                  'hours': node.tags['opening_hours'].split(';'),
                  'open': open,
+                 'type': getTypeFromTags(node.tags),
+                 'id': node.id,
                }
         results.append(item)
 
